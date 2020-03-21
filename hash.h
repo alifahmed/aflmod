6ee766d91a155d2b805d897f075025f7c1c799c4
Alif Ahmed <alifahmed@virginia.edu>
2020-03-21 16:36:17 -0400

    Changed hash function

--- hash.h
+++ hash.h
@@ -31,37 +31,25 @@
 
 #ifdef __x86_64__
 
-#define ROL64(_x, _r)  ((((u64)(_x)) << (_r)) | (((u64)(_x)) >> (64 - (_r))))
+#include <immintrin.h>
 
 static inline u32 hash32(const void* key, u32 len, u32 seed) {
 
-  const u64* data = (u64*)key;
-  u64 h1 = seed ^ len;
-
-  len >>= 3;
-
-  while (len--) {
-
-    u64 k1 = *data++;
-
-    k1 *= 0x87c37b91114253d5ULL;
-    k1  = ROL64(k1, 31);
-    k1 *= 0x4cf5ad432745937fULL;
-
-    h1 ^= k1;
-    h1  = ROL64(h1, 27);
-    h1  = h1 * 5 + 0x52dce729;
-
-  }
-
-  h1 ^= h1 >> 33;
-  h1 *= 0xff51afd7ed558ccdULL;
-  h1 ^= h1 >> 33;
-  h1 *= 0xc4ceb9fe1a85ec53ULL;
-  h1 ^= h1 >> 33;
-
-  return h1;
-
+    u64 crc = seed;
+    u64 crcCurr = seed;
+    
+    const u64* data = (u64*)key;
+    
+    len >>= 3;
+    
+    for(u32 i = 0; i < len; i++){
+        crcCurr = _mm_crc32_u64(data[i], crcCurr);
+        if(data[i]){
+            crc = crcCurr;
+        }
+    }
+    
+    return crc;
 }
 
 #else 
