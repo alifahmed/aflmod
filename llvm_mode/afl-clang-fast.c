60291e8fc55c0ed45a9df1bba35865c031d30934
Alif Ahmed <alifahmed@virginia.edu>
2020-03-20 20:30:56 -0400

    Checking linking with afl runtime

--- llvm_mode/afl-clang-fast.c
+++ llvm_mode/afl-clang-fast.c
@@ -278,7 +278,7 @@ static void edit_params(u32 argc, char** argv) {
 #endif /* ^__APPLE__ */
     "_I(); } while (0)";
 
-  if (maybe_linking) {
+  //if (maybe_linking) {
 
     if (x_set) {
       cc_params[cc_par_cnt++] = "-x";
@@ -309,7 +309,7 @@ static void edit_params(u32 argc, char** argv) {
 
     }
 
-  }
+  //}
 
   cc_params[cc_par_cnt] = NULL;
 
