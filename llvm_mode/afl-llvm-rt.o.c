7968f079c28977aa38bb08a64ebfc273807724ec
Alif Ahmed <alifahmed@virginia.edu>
2020-04-20 20:07:59 -0400

    Reverted to default version of branch key generation. Mainly to fix insufficient key issue for large program with small map

--- llvm_mode/afl-llvm-rt.o.c
+++ llvm_mode/afl-llvm-rt.o.c
@@ -272,7 +272,7 @@ void __sanitizer_cov_trace_pc_guard(uint32_t* guard) {
   const uint32_t key = curr ^ __afl_prev_loc1 ^ __afl_prev_loc2;
   __afl_area_ptr[key]++;
   __afl_prev_loc2 = __afl_prev_loc1;
-  __afl_prev_loc1 = curr;
+  __afl_prev_loc1 = (curr >> 1);
 }
 
 
